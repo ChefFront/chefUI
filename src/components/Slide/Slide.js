4d69ab6890867ffa4d3aaf647a1a8b24b0f0a305
ChefFront <caoshuai@bupt.edu.cn>
2017-10-04 19:50:29 +0800

    component list page

--- src/components/Slide/Slide.js
+++ src/components/Slide/Slide.js
@@ -1,13 +1,20 @@
 
 import React,{ Component } from "react"
+import ReactDOM from "react-dom"
 
 import "./slide.scss"
 
 class Slide extends Component{
 
+    componentDidMount() {
+        // const width = ReactDOM.get(this.container.getBoundingClientRect(), 'width');
+        const dom = ReactDOM.findDOMNode(this.refs['sender-name']);
+        console.log("$$$",dom.style)
+    }
+
     render(){
         return(
-            <div className = "slide-page">
+            <div className = "slide-page" ref="sender-name" >
 
             </div>
         )
